df67bd728a66e3e436ea04428d22babf75e171e1
Jacob <jacobgutierrez752@gmail.com>
2025-08-27 02:56:21 -0700

    Update

--- example/configs.c
+++ example/configs.c
@@ -1,55 +1,42 @@
 
-#ifdef __APPLE__
 
-#include <unistd.h>
-#include <stdbool.h>
-#include <sys/select.h>
-#include <termios.h>
-#include <stdarg.h>
 #include <stdio.h>
 
 #include "cli_config.h"
 
-
-void write_char(char c) {
-    write(STDOUT_FILENO, &c, 1);
+void cli_printf(const char* fmt, ...) {
+    va_list arg;
+    va_start(arg, fmt);
+    vprintf(fmt, arg);
+    va_end(arg);
+    fflush(stdout);
 }
 
-void write_str(const char* str, int len) {
-    write(STDOUT_FILENO, str, len);
-}
-
-void write_fmt(const char *fmt, ...) {
-    char buffer[256];
-    va_list args;
-    va_start(args, fmt);
-    vsnprintf(buffer, sizeof(buffer), fmt, args);
-    va_end(args);
-    write_str(buffer, strlen(buffer));
-}
-
-
-bool read_ready() {
-    fd_set fds;
-    struct timeval tv = {0, 0};
-    FD_ZERO(&fds);
-    FD_SET(STDIN_FILENO, &fds);
-    return select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv) > 0;
-}
-
-char read_char() {
+char buf[256];
+const char* cli_read_str(void) {
+    // printf("call read str\n");
+    int i = 0;
     char c;
-    read(STDIN_FILENO, &c, 1);
-    return c;
-}
-
-void clear_read_buf() {
-    char c;
-    while (read_ready()) {
-        read(STDIN_FILENO, &c, 1);
-        if (c == '\n') break;
+    while (i < 255) {
+        c = fgetc(stdin);
+        if (c == '\n'){
+            break;
+        }
+        buf[i++] = c;
     }
+    buf[i] = '\0';
+    return buf;
 }
 
 
-#endif // __APPLE__ 
+char cli_read_char(void) {
+    // pritnf("call read char\n");
+    return fgetc(stdin);
+}
+
+/* 
+ * fgetc always blocks so data will always be present when reading
+ */ 
+bool cli_read_ready(void) {
+    return true;
+}
