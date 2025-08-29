6881ccdc7dd0c1ef043d3b83065b7c8579130e83
Jacob <jacobgutierrez752@gmail.com>
2025-08-28 21:12:53 -0700

    Update example code

--- example/main.c
+++ example/main.c
@@ -2,30 +2,58 @@
 #include <stdio.h>
 
 #include "cli_loop.h"
+#include "cli_types.h"
 #include "cli_config.h"
+
+enum CMD_ERROR {
+    CLI_OK = 0,
+    CLI_ETEST_ONE = 1,
+    CLI_ETEST_TWO = 2,
+    CLI_ETEST_NOT_MAPPED, // This enum value is not mapped to the error table
+};
 
 int help_command(int argc, const char **argv) {
     for (int i = 0; i < argc; i++) {
-        printf("argv[%d]: \"%s\"\n", i, argv[i]);
-        printf("bytes: ");
+        cli_printf("argv[%d]: \"%s\"\n", i, argv[i]);
+        cli_printf("bytes: ");
         const unsigned char *p = (unsigned char *)argv[i];
         int j = 0;
         while (1) {
-            printf("0x%02x ", p[j]);
+            cli_printf("0x%02x ", p[j]);
             if (p[j] == '\0') break;  // stop after null terminator
             j++;
         }
-        printf("\n");
+        cli_printf("\n");
     }
+
     return 0;
 }
 
-// Example mappings
-const cmd_elem_t command_mapping[] = {
-    {"help", 0x0, help_command},
+int error_command(int argc, const char **argv) {
+    if (argc == 1) {
+        return CLI_ETEST_ONE;
+    }
+    if (argc == 2) {
+        return CLI_ETEST_TWO;
+    }
+    return CLI_ETEST_NOT_MAPPED;
+}
+
+// Example Command Error Mappings
+// Note that CLI_ETEST_NOT_MAPPED is not in this table
+const cmd_err_elem_t command_err_table[] = {
+    {CLI_ETEST_ONE, "Test Error One"},
+    {CLI_ETEST_ONE, "Test Error Two"},
+    {0, 0}
 };
 
-const int command_mapping_size = 1;
+// Example Command Mappings
+// Note that "error" has "err" as an alias
+const cmd_elem_t command_mapping[] = {
+    {"help", 0x0, help_command},
+    {"error", "err", error_command},
+    {0, 0, 0}
+};
 
 int main() {
     cli_loop();
